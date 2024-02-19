7e650fb412615f61508fc950c0a803ba645e35cb
Boris Astardzhiev <boris_astardzhiev@smartcom.bg>
2024-02-19 16:11:03 +0200

    refactor a little bit so that *mut sk_buff is wrapped into Opaque

--- samples/rust/rust_module.rs
+++ samples/rust/rust_module.rs
@@ -170,14 +170,80 @@ impl Container {
         egress_devs
     }
 
-    pub fn undo_skb_pull(frame: *mut sk_buff, how_many: usize) {
+    pub fn get_dev_name<'a>(dev: *mut net_device) -> &'a CStr {
+        unsafe { CStr::from_char_ptr(&(*dev).name as *const [i8; 16] as *const i8) }
+    }
+
+    pub unsafe extern "C" fn eth_rcv(
+        frame: *mut sk_buff,
+        dev_in: *mut net_device,
+        packet_type: *mut packet_type,
+        orig_dev: *mut net_device,
+    ) -> i32 {
+        let frame = unsafe { SkBuff::from_raw(frame) };
+        unsafe {
+            let pkt_type = frame.get_pkt_type();
+            // if we don't filter these there's a loop
+            if pkt_type == PACKET_LOOPBACK || pkt_type == PACKET_OUTGOING {
+                frame.drop_skb();
+                return 0;
+            }
+        }
+
+        // pr_info!("Received frame!\n");
+        let egress_devs = Container::get_egress_devs(dev_in);
+        egress_devs.into_iter().for_each(|dev| {
+            let nskb = frame.clone();
+            nskb.set_dev(dev);
+            // let dev_out_name = Container::get_dev_name(dev);
+            // let dev_in_name = Container::get_dev_name(dev_in);
+            // pr_info!(
+            //     "Forwarding packet from: {} to: {}\n",
+            //     dev_in_name.to_str().unwrap(),
+            //     dev_out_name.to_str().unwrap()
+            // );
+            nskb.undo_skb_pull(ETH_HLEN as usize);
+            nskb.skb_dev_queue_xmit();
+        });
+        // unsafe { netif_rx(frame) }
+        frame.drop_skb();
+        0
+    }
+}
+
+#[repr(transparent)]
+pub struct SkBuff(Opaque<sk_buff>);
+
+impl SkBuff {
+    /// Creates a new [`SkBuff`] instance from a raw pointer.
+    ///
+    /// # Safety
+    ///
+    /// For the duration of 'a, the pointer must point at a valid `sk_buff`,
+    /// and the caller must be in a context where all methods defined on this struct
+    /// are safe to call.
+    pub unsafe fn from_raw<'a>(ptr: *mut sk_buff) -> &'a mut Self {
+        // CAST: `Self` is a `repr(transparent)` wrapper around `sk_buff`.
+        let ptr = ptr.cast::<Self>();
+        // SAFETY: by the function requirements the pointer is valid and we have unique access for
+        // the duration of `'a`.
+        unsafe { &mut *ptr }
+    }
+
+    pub fn get_raw(&self) -> *mut sk_buff {
+        self.0.get()
+    }
+
+    pub fn undo_skb_pull(&mut self, how_many: usize) {
+        let frame = self.get_raw();
         unsafe {
             (*frame).data = (*frame).data.offset(-(how_many as isize));
             (*frame).len += how_many as u32;
         }
     }
 
-    pub fn get_skb_pkt_type(frame: *mut sk_buff) -> u32 {
+    pub fn get_pkt_type(&self) -> u32 {
+        let frame = self.get_raw();
         unsafe {
             let pkt_type = (*frame)
                 .__bindgen_anon_5
@@ -195,20 +261,24 @@ impl Container {
         }
     }
 
-    pub fn drop_skb(frame: *mut sk_buff) {
+    pub fn drop_skb(&mut self) {
+        let frame = self.get_raw();
         unsafe {
             kfree_skb(frame);
         }
     }
 
-    pub fn clone_skb(frame: *mut sk_buff) -> *mut sk_buff {
+    // for a lifetime of 'a give me a reference to the cloned skb
+    pub fn clone<'a, 'b>(&'b self) -> &'a mut SkBuff {
         unsafe {
+            let frame = self.get_raw();
             let nskb = skb_copy(frame, GFP_ATOMIC);
-            nskb
+            SkBuff::from_raw(nskb)
         }
     }
 
-    pub fn set_skb_dev(frame: *mut sk_buff, dev: *mut net_device) {
+    pub fn set_dev(&mut self, dev: *mut net_device) {
+        let frame = self.get_raw();
         unsafe {
             (*frame)
                 .__bindgen_anon_1
@@ -218,49 +288,11 @@ impl Container {
         }
     }
 
-    pub fn skb_dev_queue_xmit(frame: *mut sk_buff) {
+    pub fn skb_dev_queue_xmit(&mut self) {
+        let frame = self.get_raw();
         unsafe {
             dev_queue_xmit(frame);
         }
-    }
-
-    pub fn get_dev_name<'a>(dev: *mut net_device) -> &'a CStr {
-        unsafe { CStr::from_char_ptr(&(*dev).name as *const [i8; 16] as *const i8) }
-    }
-
-    pub unsafe extern "C" fn eth_rcv(
-        frame: *mut sk_buff,
-        dev_in: *mut net_device,
-        packet_type: *mut packet_type,
-        orig_dev: *mut net_device,
-    ) -> i32 {
-        unsafe {
-            let pkt_type = Container::get_skb_pkt_type(frame);
-            // if we don't filter these there's a loop
-            if pkt_type == PACKET_LOOPBACK || pkt_type == PACKET_OUTGOING {
-                Container::drop_skb(frame);
-                return 0;
-            }
-        }
-
-        // pr_info!("Received frame!\n");
-        let egress_devs = Container::get_egress_devs(dev_in);
-        egress_devs.into_iter().for_each(|dev| {
-            let nskb = Container::clone_skb(frame);
-            Container::set_skb_dev(nskb, dev);
-            // let dev_out_name = Container::get_dev_name(dev);
-            // let dev_in_name = Container::get_dev_name(dev_in);
-            // pr_info!(
-            //     "Forwarding packet from: {} to: {}\n",
-            //     dev_in_name.to_str().unwrap(),
-            //     dev_out_name.to_str().unwrap()
-            // );
-            Container::undo_skb_pull(nskb, ETH_HLEN as usize);
-            Container::skb_dev_queue_xmit(nskb);
-        });
-        // unsafe { netif_rx(frame) }
-        Container::drop_skb(frame);
-        0
     }
 }
 
